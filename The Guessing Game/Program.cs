fc823dd2da2d3700390da7939fa16be2447bf560
Brett Hudson <Brett@brett-techrepair.com>
2017-07-13 16:54:58 -0400

    The Guessing Game V 1.0

---
+++ The Guessing Game/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PlayerGuess;
+            string PlayerInput;
+            bool isNumberGuessed = false;
+            int TheAnswer;
+            Random rng = new Random();
+            TheAnswer = rng.Next(1, 21);
+
+            do
+            {
+                Console.Write("Enter Your Guess: ");
+                PlayerInput = Console.ReadLine();
+
+                PlayerGuess = int.Parse(PlayerInput);
+
+                if (PlayerGuess == TheAnswer)
+                {
+                    Console.WriteLine("YOU WON ! ! !");
+                    isNumberGuessed = true;
+                }
+                else
+                {
+                    if (PlayerGuess > TheAnswer)
+                    {
+                        Console.WriteLine("To High");
+                    }
+                    else
+                    {
+                        Console.WriteLine("To Low");
+                    }
+
+                }
+
+            } while (!isNumberGuessed);
+            Console.ReadLine();
+        }
+    }
+}
